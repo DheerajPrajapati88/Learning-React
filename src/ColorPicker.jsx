3f9426fa4862eeec4c8136551b2e99d8d5710622
Dheeraj Prajapati <dheerajp8840@gmail.com>
2024-08-13 16:21:00 +0530

    first commit

---
+++ src/ColorPicker.jsx
@@ -0,0 +1,23 @@
+import React ,{useState} from 'react'
+
+function ColorPicker() {
+
+    const [color,setColor]=useState('#dadada')
+    function handleColorChange(event){
+
+        setColor(event.target.value)
+    }
+  return (
+    <div>
+        <h1>color picker</h1>
+        <div className="container" style={{backgroundColor:color}}>
+            <h2 style={{color:'white'}}>Selected Color : {color}</h2>
+            <input type="color" value={color} name="" id=""  onChange={handleColorChange}/>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default ColorPicker
