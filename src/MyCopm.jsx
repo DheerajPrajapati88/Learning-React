d2d8c2d4addf71c59bc7d2ec07dbd7e123861660
Dheeraj Prajapati <dheerajp8840@gmail.com>
2024-08-14 16:00:38 +0530

    adding and deleting opeation by taking some input

--- src/MyCopm.jsx
+++ src/MyCopm.jsx
@@ -1,41 +1,69 @@
 import React, { useState } from "react";
 
 function MyCopm() {
-  const [food, setFoods] = useState([
-    "Apple",
-    "Banana",
-    "Grapes",
-    "Cherry",
-    "Orange",
-  ]);
+  const [cars, setCars] = useState([]);
+  const [carsYear, setCarYear] = useState(new Date().getFullYear());
+  const [carsMake, setCarsMake] = useState("");
+  const [carsModel, setCarsModel] = useState("");
 
-  function handleAddFood() {
+  function handleAddCar() {
+    const newCar = { year: carsYear, make: carsMake, model: carsModel };
+
+    setCars(c => [...c, newCar]);
+    setCarYear(new Date().getFullYear())
+    setCarsMake("")
+    setCarsModel('')
+  }
+  function handleRemoveCar(index) {
+
    
-    const newFood=document.getElementById('foodInput').value;
-    document.getElementById('foodInput').value='';
-
-    setFoods(f=>[...f,newFood])
-  }
-
-  function HandleRemoveFood(index) {
-
-    setFoods(food.filter((_,i)=>{
+    setCars(c => c.filter((_,i)=>{
       return i!==index
-    }))
+    }));
+
+  }
+  function handleYearChange(event) {
+    setCarYear(event.target.value);
   }
 
+  function handleMakeChange(event) {
+    setCarsMake(event.target.value);
+  }
+  function handleModelChange(event) {
+    setCarsModel(event.target.value);
+  }
   return (
     <div>
-      <h1>List of Foof</h1>
+      <h2>List of car objects</h2>
       <ul>
-        {food.map((e, index) => (
-          <li key={index} onClick={()=>HandleRemoveFood(index)}>{e}</li>
-        ))}
+        {cars.map((car, index) => {
+          return (
+            <li onClick={()=>handleRemoveCar(index)} style={{fontSize:'20px'}} key={index}>
+              {car.year} .
+               {car.make} .
+               {car.model} 
+            </li>
+          );
+        })}
       </ul>
+      <input type="number" value={carsYear} onChange={handleYearChange} />
+      <br />
+      <input
+        type="text"
+        value={carsMake}
+        onChange={handleMakeChange}
+        placeholder="Enter car Make"
+      />
+      <br />
+      <input
+        type="text"
+        value={carsModel}
+        onChange={handleModelChange}
+        placeholder="Enter car Model"
+      />
 
-      <input type="text" id="foodInput" placeholder="Add food..." />
-
-      <button onClick={handleAddFood}>add</button>
+      <br />
+      <button onClick={handleAddCar}>Add Car</button>
     </div>
   );
 }
